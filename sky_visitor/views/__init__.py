39558c424c134936b738fd66e525124bc6c18a88
Ian KP <iposs@concentricsky.com>
2014-06-26 17:44:24 -0700

    Django 1.6 no longer sets a users_cache property on the form within a clean_email() on the PasswordResetForm.

--- sky_visitor/views/__init__.py
+++ sky_visitor/views/__init__.py
@@ -14,6 +14,7 @@
 from django.contrib import auth
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import resolve_url
